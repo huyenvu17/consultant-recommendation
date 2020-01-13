4bbabbb1e42e011b6d71c174a6a083c8673eff43
Huyen Vu Ngoc <huyenvu.vnh@outlook.com>
2020-01-13 17:34:04 +0700

    add second and last steps

--- consultancy.js
+++ consultancy.js
@@ -2,6 +2,8 @@ var consultancy = (function () {
     var initConsultancy  = function () {
         dobPicker();
         itemSelect();
+        textInputCount();
+        budgetPlan();
     };
 
     function dobPicker() {
@@ -19,7 +21,33 @@ var consultancy = (function () {
     function itemSelect() {
         $('select').formSelect();
     }
-    
+    function textInputCount() {
+        $('textarea#note').characterCounter();
+    }
+    function budgetPlan() {
+        var slider = document.getElementById('test-slider');
+        noUiSlider.create(slider, {
+            start: [0, 5000000],
+            connect: true,
+            step: 500000,
+            tooltips: true,
+            orientation: 'horizontal', // 'horizontal' or 'vertical'
+            range: {
+                'min': 3000000,
+                'max': 12000000
+            },
+            format: wNumb({
+                decimals: 0,
+                postfix: 'đ',
+                thousand: ',',
+            }),
+            pips: {
+                mode: 'range',
+                stepped: true,
+                density: 4
+            }
+        });
+    }
     return {
         initConsultancy
     }
