d3066e6f1bf8ee48956a5d1b0b064b4bc533c529
Cong <congnt.uit@gmail.com>
2020-01-16 16:42:39 +0700

    commit 2: edit layout

--- consultancy.js
+++ consultancy.js
@@ -5,7 +5,6 @@ var consultancy = (function () {
         text_input_count();
         budget_plan();
         personal_info();
-        personalized_learning_course();
         result_form();
         initTabs();
         initTimeTable();
@@ -52,7 +51,66 @@ var consultancy = (function () {
                 setDefaultDate: new Date(2000, 0, 0),
                 maxDate: new Date(currYear - 10, 12, 31),
                 yearRange: [1970, currYear - 10],
-                format: "dd/mm/yyyy"
+                format: "dd/mm/yyyy",
+                i18n: {
+                    cancel: "Hủy",
+                    done: 'Chọn',
+                    months: [
+                        'Thg 1',
+                        'Thg 2',
+                        'Thg 3',
+                        'Thg 4',
+                        'Thg 5',
+                        'Thg 6',
+                        'Thg 7',
+                        'Thg 8',
+                        'Thg 9',
+                        'Thg 10',
+                        'Thg 11',
+                        'Thg 12',
+                    ],
+                    monthsShort: [
+                        'Tháng 1',
+                        'Tháng 2',
+                        'Tháng 3',
+                        'Tháng 4',
+                        'Tháng 5',
+                        'Tháng 6',
+                        'Tháng 7',
+                        'Tháng 8',
+                        'Tháng 9',
+                        'Tháng 10',
+                        'Tháng 11',
+                        'Tháng 12',
+                    ],
+                    weekdays: [
+                        'Thứ hai',
+                        'Thứ ba',
+                        'Thứ tư',
+                        'Thứ năm',
+                        'Thứ sáu',
+                        'Thứ bảy',
+                        'Chủ nhật'
+                    ],
+                    weekdaysShort: [
+                        'Thứ 2',
+                        'Thứ 3',
+                        'Thứ 4',
+                        'Thứ 5',
+                        'Thứ 6',
+                        'Thứ 7',
+                        'Chủ Nhật'
+                    ],
+                    weekdaysAbbrev: [
+                        'T2',
+                        'T3',
+                        'T4',
+                        'T5',
+                        'T6',
+                        'T7',
+                        'CN'
+                    ]
+                }
             }
         );
     }
@@ -164,72 +222,148 @@ var consultancy = (function () {
         return $rs;
     }
 
+    var pipFormats = {'2500000':'< 3,000,000', '12500000':'> 12,000,000'};
+    var tooltipFormats = function(a){
+        if (a < '3000000')
+            return '< 3,000,000 VNĐ';
+        else if (a == '12500000')
+            return '> 12,000,000 VNĐ';
+            else return wNumb({
+                decimals: 0,
+                postfix: ' VNĐ',
+                thousand: ',',
+            }).to(a);
+    };
+
+    function toNumber(a){
+        var str = "";
+
+        if (a){
+            a.split('').map((r, i) => {
+                if($.isNumeric(r))
+                    str += r;
+            });
+        }
+        return str;
+    }
+
+    function shortenNumber(a){
+        if(a == '> 12,000,000 VNĐ' || a == '< 3,000,000 VNĐ')
+            return '';
+        var now = toNumber(a);
+
+        var pow = Math.pow, floor = Math.floor, abs = Math.abs, log = Math.log;
+        var abbrev = 'KMB';
+
+        function round(n, precision) {
+            var prec = Math.pow(10, precision);
+            return Math.round(n*prec)/prec;
+        }
+
+        function format(n) {
+            var base = floor(log(abs(n))/log(1000));
+            var suffix = abbrev[Math.min(2, base - 1)];
+            base = abbrev.indexOf(suffix) + 1;
+            return suffix ? round(n/pow(1000,base),2)+suffix : ''+n;
+        }
+
+        return format(now);
+    }
 
     function budget_plan() {
         var slider = document.getElementById('test-slider');
         noUiSlider.create(slider, {
-            start: [0, 5000000],
+            start: [3000000, 6000000],
             connect: true,
             step: 500000,
-            tooltips: true,
+            tooltips: [true, {
+                to: function(a){
+                    return tooltipFormats(a);
+                }
+            }],
             orientation: 'horizontal',
             range: {
-                'min': 3000000,
-                'max': 12000000
+                'min': 2500000,
+                'max': 12500000
             },
-            format: wNumb({
-                decimals: 0,
-                postfix: 'đ',
-                thousand: ',',
-            }),
+            format: {
+                to: function(a){
+                    return tooltipFormats(a);
+                },
+                from: function(a){
+                    return (a);
+                }
+            },
             pips: {
                 mode: 'range',
-                stepped: true,
-                density: 4
+                format: {
+                    to: function(a){
+                        return pipFormats[a];
+                    }
+                }
             }
         });
-    }
-    function personal_info() {
-        $('.js-next').click(function () {
-            var step1 = $(".steps-panel li").eq(0);
-            setTimeout(function(){
-                $('#step2').find('.skeleton-loading').css('display', 'none');
-                $('#step2').find('.step-content').css('display', 'block');
-            }, 1000);
-            $('#step2').fadeIn('slow');
-            $('#step1').fadeOut();
-            $('.active').next().addClass('active');
-            step1.removeClass('active');
 
+        $('.noUi-handle').append('<div class="cur-value"></div>');
+
+        var value = slider.noUiSlider.get();
+        $('.cur-value').each((index, r) => {
+            $(r).text(shortenNumber(value[index]));
+        })
+
+        slider.noUiSlider.on('end.one', function () { 
+            var value = slider.noUiSlider.get();
+
+            $('.cur-value').each((index, r) => {
+                $(r).text(shortenNumber(value[index]));
+            })
+        });
+    }
+
+    function changeTab(from, to){
+        from.removeClass('active');
+        to.addClass('active');
+
+        divFrom = $(from.find('a').attr("href"));
+        divTo = $(to.find('a').attr("href"));
+
+        
+        divFrom.fadeOut(200);
+        divTo.fadeIn(300);
+
+        divTo.find('.skeleton-loading').css('display', 'block');
+        divTo.find('.step-content').css('display', 'none');    
+
+        window.scrollTo({top: 0, behavior: 'smooth'});
+        setTimeout(function(){
+            divTo.find('.skeleton-loading').css('display', 'none');
+            divTo.find('.step-content').css('display', 'block');    
+        }, 500);
+    }
+
+    function personal_info() {
+        var stepsS = ['step1', 'step2'];
+        var steps = [$(".steps-panel li").eq(0), $(".steps-panel li").eq(1)];
+        
+        $('.js-next').click(function () {
+            var step = $(this).closest('.step-content').parent().attr('id');
+            var indFrom = stepsS.indexOf(step);
+            var indTo = indFrom + 1 >= steps.length ? steps.length : indFrom + 1;
+
+            changeTab(steps[indFrom], steps[indTo]);
         });
         $('.btn-reset').click(function () {
             $("input").val('');
         });
-    }
-    function personalized_learning_course() {
-        var step1 = $(".steps-panel li").eq(0);
-        var step2 = $(".steps-panel li").eq(1);
-        $('.js-personal-info').click(function () {
-            $('.active').next().addClass('active active1');
-            step2.removeClass('active');
-            setTimeout(function(){
-                $('#step2').find('.skeleton-loading').css('display', 'none');
-                $('#step3').find('.step-content').css('display', 'block');
-            }, 1000);
-            $('#step3').fadeIn('slow');
-            $('#step2').hide();
-        });
         $('.btn-prev').click(function () {
-            step2.removeClass('active');
-            step1.addClass('active');
-            setTimeout(function(){
-                $('#step1').find('.skeleton-loading').css('display', 'none');
-                $('#step1').find('.step-content').css('display', 'block');
-            }, 1000);
-            $('#step1').fadeIn('slow');
-            $('#step2').fadeOut();
+            var step = $(this).closest('.step-content').parent().attr('id');
+            var indFrom = stepsS.indexOf(step);
+            var indTo = indFrom - 1 >= 0 ? indFrom - 1 : 0;
+
+            changeTab(steps[indFrom], steps[indTo]);
         });
     }
+
     function result_form() {
         $(".js-result-form").click(function() { 
             window.location.replace('consultancy-result.html')
